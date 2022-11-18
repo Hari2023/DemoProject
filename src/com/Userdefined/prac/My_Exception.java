d5c1f12695a015c99be23fec149226b38ebce413
Hari2023 <haribaskarg93@gmail.com>
2022-11-18 12:35:00 +0530

    Code Add

---
+++ src/com/Userdefined/prac/My_Exception.java
@@ -0,0 +1,9 @@
+package com.Userdefined.prac;
+
+public class My_Exception extends Exception {
+
+	public My_Exception(String string) {
+		System.out.println(string);
+	}
+
+}
---
+++ src/com/Userdefined/prac/Office_Time.java
@@ -0,0 +1,32 @@
+package com.Userdefined.prac;
+
+public class Office_Time {
+	
+	public static void main(String[] args) throws My_Exception {
+		int a = 18;
+		
+		
+		if (a>=16) {
+			System.out.println("Office Time");
+			
+		} else {
+		  
+throw new My_Exception("Reason");
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
