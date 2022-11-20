20955c8d4a956d3484074fa0b7a41169b6c653c1
Hari2023 <118505070+Hari2023@users.noreply.github.com>
2022-11-20 15:22:47 +0530

    Update My_Exception.java
    Added line

--- src/com/Userdefined/prac/My_Exception.java
+++ src/com/Userdefined/prac/My_Exception.java
@@ -4,7 +4,7 @@ public class My_Exception extends Exception {
 
 	public My_Exception(String string) {
 		System.out.println(string);
-		System.out.println("Exception Handle");
+		System.out.println("Exception");
 	}
 
 }
